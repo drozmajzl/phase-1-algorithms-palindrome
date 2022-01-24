499696d2b5b52f2e1186727891f293614fa098ac
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-01-24 18:44:35 -0500

    commit tree d7bfe06139bbf84a4ff93977256f075c93de1590

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let i = 0
+  const wordArray = []
+  do{
+    wordArray.unshift(word[i])
+    i++
+  }while (i != word.length)
+ const reverse = wordArray.join('')
+ 
+ console.log(word)
+ console.log(reverse)
+ return (word.toUpperCase() === reverse.toUpperCase())
 }
 
 /* 
   Add your pseudocode here
 */
-
+//??????????
 /*
   Add written explanation of your solution here
 */
+// Word is passed to isPalindrome(), then an empty array is declared. Using a do...while loop, the word is unshifted to the empty array.
+// The empty array now contains the letters of the original word in reverse.
+// The array is then converted back to a string, and the new string is then compared to the original word in a boolean statement
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
